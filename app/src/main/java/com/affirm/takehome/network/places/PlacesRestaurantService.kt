dd67e733319e567f750544e8736a814e404d5b2d
Alireza Nezami <alireza.nezami75@gmail.com>
2024-09-06 11:31:38 +0330

    feat/add suspend to the api declarations, add toDomainModel extension functions

--- app/src/main/java/com/affirm/takehome/network/places/PlacesRestaurantService.kt
+++ app/src/main/java/com/affirm/takehome/network/places/PlacesRestaurantService.kt
@@ -1,6 +1,5 @@
 package com.affirm.takehome.network.places
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,13 +7,13 @@ import retrofit2.http.Query
 interface PlacesRestaurantService {
 
   @GET("nearbysearch/json")
-  fun getPlacesRestaurants(
+  suspend fun getPlacesRestaurants(
     @Query("location") location: String,
     @Query("pagetoken") pageToken: String,
     @Query("key") key: String = KEY,
     @Query("rankby") rankBy: String = "distance",
     @Query("type") type: String = "restaurant"
-  ): Call<PlacesResponse>
+  ): PlacesResponse
 
   companion object {
     internal const val KEY = "AIzaSyAcsY9zVrBham7BwJQzRNmtKfOkgtDPZsQ"
--- app/src/main/java/com/affirm/takehome/network/yelp/YelpResponse.kt
+++ app/src/main/java/com/affirm/takehome/network/yelp/YelpResponse.kt
@@ -1,9 +1,22 @@
 package com.affirm.takehome.network.yelp
 
+import com.affirm.takehome.data.Restaurant
+import com.affirm.takehome.network.places.PlacesResponse
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class YelpResponse(
-  @SerialName("businesses") val restaurants: List<YelpRestaurant> = listOf()
+    @SerialName("businesses") val restaurants: List<YelpRestaurant> = listOf()
 )
+
+fun YelpResponse.toDomainModel(): List<Restaurant> {
+    return this.restaurants.map {
+        Restaurant(
+            id = it.id,
+            name = it.name,
+            image = it.image,
+            rating = it.rating
+        )
+    }
+}
